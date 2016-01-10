45e4d91b9b0782670ea5dd7a32c3831b5a7603d3
Michael Ramirez <ramirez3494@gmail.com>
2016-01-10 02:11:07 -0500

    Update cask script

--- init/30_osx_homebrew_casks.sh
+++ init/30_osx_homebrew_casks.sh
@@ -7,14 +7,11 @@ is_osx || return 1
 # Ensure the cask keg and recipe are installed.
 kegs=(caskroom/cask homebrew/versions caskroom/fonts homebrew/completions caskroom/versions)
 brew_tap_kegs
-recipes=(brew-cask)
-brew_install_recipes
+# recipes=(brew-cask)
+# brew_install_recipes
 
 # Exit if, for some reason, cask is not installed.
 # [[ ! "$(brew ls --versions brew-cask)" ]] && e_error "Brew-cask failed to install." && return 1
-
-# Hack to show the first-run brew-cask password prompt immediately.
-brew cask info this-is-somewhat-annoying 2>/dev/null
 
 # Homebrew casks
 casks=(
