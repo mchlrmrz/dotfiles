2a273e2962954a62141d138a3343d944f3407472
Michael Ramirez <ramirez3494@gmail.com>
2016-03-12 20:14:18 -0500

    Update brew settings.

--- source/50_osx.sh
+++ source/50_osx.sh
@@ -2,14 +2,15 @@
 is_osx || return 1
 
 # APPLE, Y U PUT /usr/bin B4 /usr/local/bin?!
-PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
-export PATH
+# PATH="/usr/local/bin:$(path_remove /usr/local/bin)"
+# export PATH
 
 # Trim new lines and copy to clipboard
 alias c="tr -d '\n' | pbcopy"
 
 # Specify your defaults in this environment variable
 export HOMEBREW_CASK_OPTS="--appdir=/Applications"
+export HOMEBREW_VERBOSE=true
 
 # Set architecture flags
 export ARCHFLAGS="-arch x86_64"
